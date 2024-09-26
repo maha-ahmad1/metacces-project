6a27d1625c5109b49411aaaaa935b1a8fb3912f1
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2024-09-26 16:53:31 +0300

    add the last edit

--- src/app/components/Form/index.jsx
+++ src/app/components/Form/index.jsx
@@ -7,10 +7,10 @@ import { useState } from "react";
 
 export default function SignupForm() {
   const [selectedOption, setSelectedOption] = useState("Male");
-
+  const [isFocused, setIsFocused] = useState(false);
   return (
     <div className=" w-full h-screen flex flex-col justify-between p-5">
-      <div className=" text-white flex-grow">
+      <div className=" text-white ">
         <Titel />
         <div className="mt-6">
           <motion.div
@@ -42,7 +42,7 @@ export default function SignupForm() {
               </i>
               <style jsx>{`
                 .rounded-shape {
-                  border-radius: 24px;
+                  border-radius: 25px;
                   clip-path: polygon(
                     4% 0,
                     96% 0,
@@ -56,7 +56,110 @@ export default function SignupForm() {
                 }
               `}</style>
             </div>
-
+            {/* <div className="relative mb-4">
+              <input
+                id="username"
+                type="text"
+                placeholder="Username"
+                autoComplete="off"
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                className={`w-full rounded-shape px-12 py-4 inset-[2px] !bg-[#080717]   text-[#6C6B70] text-md transition-colors duration-500 ease-in-out focus:outline-none
+                ${
+                  isFocused
+                    ? "inset-0 rounded-custom ring-2  ring-gradient-to-r ring-blue-500 ring-purple-500"
+                    : "bg-transparent border-2 border-[#6C6B70]"
+                }`}
+              />
+              <i className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white">
+                <Image
+                  width={25}
+                  height={30}
+                  src="/assets/email.png"
+                  alt="email"
+                  className="text-white"
+                />
+              </i>
+              <style jsx>{`
+                .rounded-shape {
+                  border-radius: 25px;
+                  clip-path: polygon(
+                    4% 0,
+                    96% 0,
+                    100% 26%,
+                    100% 74%,
+                    96% 100%,
+                    4% 100%,
+                    0 74%,
+                    0 26%
+                  );
+                }
+              `}</style>
+            </div> */}
+            {/* <div className="relative mb-4">
+              <div className={`relative ${isFocused ? "gradient-border" : ""}`}>
+                <input
+                  id="username"
+                  type="text"
+                  className="w-full !bg-[#080717] border-2 border-[#6C6B70] text-[#6C6B70] text-md rounded-custom px-12 py-4 text-md focus:outline-none"
+                  placeholder="Username"
+                  autoComplete="off"
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
+                />
+                <div className="absolute inset-[2px] rounded-custom bg-[#080717] -z-10"></div>
+              </div>
+              <i className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white z-10">
+                <Image
+                  width={25}
+                  height={30}
+                  src="/assets/email.png"
+                  alt="user icon"
+                  className="text-white"
+                />
+              </i>
+              <style jsx>{`
+                .rounded-custom {
+                  border-radius: 25px;
+                  clip-path: polygon(
+                    4% 0,
+                    96% 0,
+                    100% 26%,
+                    100% 74%,
+                    96% 100%,
+                    4% 100%,
+                    0 74%,
+                    0 26%
+                  );
+                }
+                .gradient-border {
+                  background: linear-gradient(
+                    to right,
+                    #5f9fe7,
+                    #4ae1d2,
+                    #e07dfe
+                  );
+                  padding: 2px;
+                }
+                .gradient-border::before {
+                  content: "";
+                  position: absolute;
+                  inset: 0;
+                  border-radius: 25px;
+                  padding: 2px;
+                  background: linear-gradient(
+                    to right,
+                    #5f9fe7,
+                    #4ae1d2,
+                    #e07dfe
+                  );
+                  -webkit-mask: linear-gradient(#fff 0 0) content-box,
+                    linear-gradient(#fff 0 0);
+                  -webkit-mask-composite: xor;
+                  mask-composite: exclude;
+                }
+              `}</style>
+            </div> */}
             <div className="flex justify-between items-center">
               <ToggleButton
                 onSelect={(option) => setSelectedOption(option)}
--- src/app/components/ToggleButton/index.jsx
+++ src/app/components/ToggleButton/index.jsx
@@ -1,61 +1,53 @@
-"use client";
 
-export const ToggleButton = ({ options = [], onSelect, selectedOption }) => {
+
+import React from "react";
+
+export default function ToggleButton({
+  options = [],
+  onSelect,
+  selectedOption,
+}) {
   return (
     <>
       {options.map((option, index) => (
-        <div
-          key={index}
-          className="relative w-1/2 h-full p-1 z-10 rounded-shape"
-        >
+        <div key={index} className="relative w-1/2 h-full p-1 z-10">
           <button
             onClick={() => onSelect(option)}
-            className={`font-worksans rounded-shape border-2 border-[#6C6B70] w-full relative p-4 text-center z-10 font-semibold text-lg transition-colors duration-500 ease-in-out outline-none ${
-              option === selectedOption
-                ? "text-white hidden-bg "
-                : "text-[#6C6B70] "
+            className={`font-worksans rounded-shape  w-full relative p-4 text-center z-10 font-semibold text-lg transition-colors duration-500 ease-in-out outline-none ${
+              option === selectedOption ? "text-white" : "text-[#6C6B70]"
             }`}
           >
             <span className="relative z-10">{option}</span>
             <div
-              className={`absolute inset-0 rounded-shape bg-gradient-to-r ${
+              className={`absolute inset-0 rounded-custom ${
                 option === selectedOption
                   ? option === "Male"
-                    ? "from-[#5F9FE7] to-[#4AE1D2]"
-                    : "from-[#5F9FE7] to-[#E07DFE]"
-                  : ""
-              }`}
-          
-            ></div>
-            <div
-              className={`absolute inset-[2px] rounded-shape transition-colors duration-300 ease-in-out ${
-                option === selectedOption ? "bg-[#312E52] " : ""
+                    ? "bg-gradient-to-r from-[#5F9FE7] to-[#4AE1D2]"
+                    : "bg-gradient-to-r from-[#5F9FE7] to-[#E07DFE]"
+                  : "bg-transparent border-2 border-[#6C6B70]"
               }`}
             ></div>
+            {option === selectedOption && (
+              <div className="absolute inset-[2px] rounded-custom bg-[#312E52]"></div>
+            )}
           </button>
-          <style jsx>{`
-            .rounded-shape {
-              border-radius: 24px;
-              clip-path: polygon(
-                4% 0,
-                96% 0,
-                100% 26%,
-                100% 74%,
-                96% 100%,
-                4% 100%,
-                0 74%,
-                0 26%
-              );
-            }
-            .hidden-bg {
-              background: transparent;
-              border: none;
-            }
-          `}</style>
         </div>
       ))}
+      <style jsx>{`
+        .rounded-custom {
+          border-radius: 24px;
+          clip-path: polygon(
+            4% 0,
+            96% 0,
+            100% 26%,
+            100% 74%,
+            96% 100%,
+            4% 100%,
+            0 74%,
+            0 26%
+          );
+        }
+      `}</style>
     </>
   );
-};
-
-export default ToggleButton;
+}
