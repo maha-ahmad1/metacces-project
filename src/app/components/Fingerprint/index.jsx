633ab39429d3d616b807490ac78234a338f5976a
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2024-09-26 12:33:07 +0300

    edit the layout

--- src/app/components/Fingerprint/index.jsx
+++ src/app/components/Fingerprint/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Image from "next/image";
 const Fingerprint = () => {
+  const handleContextMenu = (e) => {
+    e.preventDefault();   
+  };
   return (
-    <div className="mt-auto">
+    <div className="fixed inset-x-0 bottom-0 flex justify-center items-end p-5 bg-transparent">
       <Image
         priority={true}
         width={30}
@@ -10,6 +13,7 @@ const Fingerprint = () => {
         src="/assets/Layer_1.png"
         alt="Fingerprint"
         className="w-24 h-24"
+        onContextMenu={handleContextMenu}
       />
     </div>
   );
--- src/app/components/SplashScreen/index.jsx
+++ src/app/components/SplashScreen/index.jsx
@@ -2,48 +2,39 @@
 
 import { useState, useEffect } from "react";
 import SignupForm from "../Form";
-import Image from "next/image";
 
 const SplashScreen = () => {
   const [loading, setLoading] = useState(true);
-  // const [showSecondLayer, setShowSecondLayer] = useState(true);
 
   useEffect(() => {
-    // const secondLayerTimer = setTimeout(() => {
-    //   setShowSecondLayer(false);
-    // }, 4800);
-
     const timer = setTimeout(() => {
       setLoading(false);
     }, 5000);
 
     return () => {
-      // clearTimeout(secondLayerTimer);
       clearTimeout(timer);
     };
   }, []);
 
   if (loading) {
     return (
-      <div className="relative flex justify-center items-center h-screen bg-[#080717]">
-        {loading && (
-          <div className="relative w-full h-full">
-            <video
-              className="absolute w-full h-full object-cover opacity-80"
-              autoPlay
-              loop
-              muted
-              src="/assets/MetaccesLayer1.webm"
-            />
-            <video
-              className="absolute w-full h-full object-cover opacity-100 mix-blend-screen"
-              autoPlay
-              loop
-              muted
-              src="/assets/MetaccesLayer2.webm"
-            />
-          </div>
-        )}
+      <div className="relative flex justify-center items-center w-screen h-screen overflow-hidden bg-[#080717]">
+        <div className="absolute top-0 left-0 w-full h-full">
+          <video
+            className="w-full h-full object-cover opacity-80"
+            autoPlay
+            loop
+            muted
+            src="/assets/MetaccesLayer1.webm"
+          />
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover opacity-100 mix-blend-screen"
+            autoPlay
+            loop
+            muted
+            src="/assets/MetaccesLayer2.webm"
+          />
+        </div>
       </div>
     );
   }
