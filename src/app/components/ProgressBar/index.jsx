2f0cd08b454c66c42073ddf9a91371c4febcbdc3
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2024-10-01 14:44:28 +0300

    edit the border progress

--- src/app/components/ProgressBar/index.jsx
+++ src/app/components/ProgressBar/index.jsx
@@ -1,21 +1,21 @@
-import Border from "../Border";
-
+// import Border from "../Border";
+import BorderPogress from "../BorderPogress";
 const ProgressBar = ({ progress }) => {
   return (
     <div>
-      <Border
+      <BorderPogress
         image={
           <div className="">
             <div
-              className="absolute inset-0 rounded-custom "
+              className="absolute inset-0 rounded-progress "
               style={{
                 width: `${progress}%`,
                 background:
                   " linear-gradient(to right, #5F9FE7, #4AE1D2, #E07DFE, #FF1FCA)",
               }}
             ></div>
-            <div className=" flex items-center justify-center absolute -inset-[2px] rounded-custom ">
-              <p className="text-white rounded-custom">{progress}%</p>
+            <div className=" flex items-center justify-center absolute -inset-[2px] rounded-progress ">
+              <p className="text-white rounded-progress">{progress}%</p>
             </div>
           </div>
         }
